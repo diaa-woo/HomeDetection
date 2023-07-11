f942cbcad19c0a3302b02b63e2be9d553b2b8df5
diadntjr <dev.dntjr0501@gmail.com>
2023-07-11 13:44:17 +0900

    Update color_seperate
    - Add HSV green filter
    - VediaCapture

---
+++ color_seperate.py
@@ -0,0 +1,17 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while cv2.waitKey(33) < 0 :
+  ret, frame = capture.read()
+  hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+  h, s, v = cv2.split(hsv)
+  h = cv2.inRange(h, 32, 60)
+
+  cone = cv2.bitwise_and(hsv, hsv, mask=h)
+  cone = cv2.cvtColor(cone, cv2.COLOR_HSV2BGR)
+
+  cv2.imshow("VideoFrame", frame)
+  cv2.imshow("cone", cone)
