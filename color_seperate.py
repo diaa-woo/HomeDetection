f0669add9e5448d39ed925ec1020cf274e97f21d
diadntjr <dev.dntjr0501@gmail.com>
2023-07-11 19:48:57 +0900

    Update color_seperate
    - Set inRange() values
    - Remove Track Bar

--- color_seperate.py
+++ color_seperate.py
@@ -6,24 +6,13 @@ capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-def nothing() :
-  pass
-
-cv2.namedWindow('HSV track Bar')
-cv2.createTrackbar('Value low value', 'HSV track Bar', 0, 255, nothing)
-cv2.createTrackbar('Value high value', 'HSV track Bar', 0, 255, nothing)
-
-cv2.setTrackbarPos('Value low value', 'HSV track Bar', 20)
-cv2.setTrackbarPos('Value high value', 'HSV track Bar', 255)
-
-
 # Running part
 while cv2.waitKey(33) < 0 :
   ret, frame = capture.read()
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-  lvalue = np.array([42, 103, cv2.getTrackbarPos('Value low value', 'HSV track Bar')])      #20
-  rvalue = np.array([61, 255, cv2.getTrackbarPos('Value high value', 'HSV track Bar')])
+  lvalue = np.array([42, 103, 40])    
+  rvalue = np.array([61, 255, 255])
 
   mask_green = cv2.inRange(hsv, lvalue, rvalue)
   kernel = np.ones((7,7),np.uint8)
