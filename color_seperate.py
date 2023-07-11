d1524e6fc060c6e7ce71356315a6676c21ab9450
diadntjr <dev.dntjr0501@gmail.com>
2023-07-11 19:31:21 +0900

    Update color_seperate
    - Add Color mask code

--- color_seperate.py
+++ color_seperate.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Camera Setting
 capture = cv2.VideoCapture(0)
@@ -10,9 +11,13 @@ while cv2.waitKey(33) < 0 :
   ret, frame = capture.read()
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
   h, s, v = cv2.split(hsv)
-  h = cv2.inRange(h, 42, 61)
+  mask_green = cv2.inRange(h, 42, 61)
+  kernel = np.ones((7,7),np.uint8)
 
-  cone = cv2.bitwise_and(hsv, hsv, mask=h)
+  mask_green = cv2.morphologyEx(mask_green, cv2.MORPH_CLOSE, kernel)
+  mask_green = cv2.morphologyEx(mask_green, cv2.MORPH_OPEN, kernel)
+
+  cone = cv2.bitwise_and(hsv, hsv, mask=mask_green)
   cone = cv2.cvtColor(cone, cv2.COLOR_HSV2BGR)
 
   cv2.imshow("VideoFrame", frame)
